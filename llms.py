be28f65d1903ccff7ddcfcba9dc4140c15c7fd89
jhsansom <jhsansom@gmail.com>
2024-03-25 10:20:06 -0400

    Added initial watermark code

--- llms.py
+++ llms.py
@@ -1,10 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import random
 
-class HuggingFaceModel:
+class LLM:
     def __init__(self, model_name):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.vocab_size = len(self.tokenizer)
 
     def str_to_tokenlist(self, input_str):
         return self.tokenizer.tokenize(input_str)
@@ -13,14 +15,32 @@ class HuggingFaceModel:
         return self.tokenizer.convert_tokens_to_string(token_list)
 
     def tokenlist_to_idlist(self, token_list):
-        return self.tokenizer.convert_tokens_to_ids(token_list)
+        return torch.tensor(self.tokenizer.convert_tokens_to_ids(token_list))
 
     def idlist_to_tokenlist(self, id_list):
         return self.tokenizer.convert_ids_to_tokens(id_list)
 
+    # Return the logit distribution over next possible tokens
     def get_logits(self, id_list):
-        input_ids = torch.tensor([id_list])
         with torch.no_grad():
-            outputs = self.model(input_ids)
+            # Add a batch dimension if necessary
+            if len(id_list.shape) < 2:
+                id_list = id_list.unsqueeze(0)
+
+            outputs = self.model(id_list)
             logits = outputs.logits
         return logits[0, -1, :]
+
+    # Returns a probability distribution given logits
+    def logits_to_probs(self, logits, temp=1):
+        return torch.nn.functional.softmax(logits/temp)
+
+    def decode(self, probs, top_k=-1):
+        if top_k > 0:
+            ids = torch.argsort(probs, descending=True)[:top_k]
+            probs = probs[:top_k]
+        else:
+            ids = torch.tensor([i for i in range(self.vocab_size)])
+        
+        return random.choices(ids, weights=probs)[0]
+
--- watermark.py
+++ watermark.py
@@ -1,14 +1,55 @@
 import llms
+import torch
 
 class WatermarkedLLM:
 
-    def __init__(self, llm):
+    def __init__(self, llm, seed):
         self.llm = llm
+        self.alpha = 0.01 # Relative proportion of watermark in overall prob
 
-    def generate_watermark(self):
-        pass
+        self.generate_watermark(seed)
 
-    
+    def generate_watermark(self, seed):
+        g = torch.Generator()
+        g.manual_seed(seed)
+        watermark = torch.rand((self.llm.vocab_size,), generator=g)
+        self.watermark = torch.nn.functional.softmax(watermark)
+
+    def apply_watermark(self, probs):
+        return ((1 - self.alpha) * probs) + (self.alpha * self.watermark)
+
+    def autoregress_ids(self, input_ids, gen_len=20):
+        for i in range(gen_len):
+            logits = self.llm.get_logits(input_ids)
+            probs = self.llm.logits_to_probs(logits)
+            probs = self.apply_watermark(probs)
+            out_id = self.llm.decode(probs)
+
+            token = self.llm.tokenizer.decode(out_id)
+            print(f'Output at step {i} is {out_id}: {token}')
+
+            input_ids = torch.cat((input_ids, out_id.unsqueeze(0)))
+
+        return input_ids
+
+
 
 if __name__ == '__main__':
-    llm = llms.HuggingFaceModel()
+    model_name = 'huggyllama/llama-7b'
+    #model_name = 'HuggingFaceH4/tiny-random-LlamaForCausalLM'
+    llm = llms.LLM(model_name)
+
+    SEED = 12345
+    wllm = WatermarkedLLM(llm, SEED)
+
+    test_str = 'Hello my name is '
+    token_list = llm.str_to_tokenlist(test_str)
+    id_list = llm.tokenlist_to_idlist(token_list)
+    id_list = wllm.autoregress_ids(id_list)
+
+    token_list = llm.idlist_to_tokenlist(id_list)
+    out_str = llm.tokenlist_to_str(token_list)
+
+    print(f'Final output: {out_str}')
+
+
